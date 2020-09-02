f51331c25ee2c856f6c337f2063d0755bf79c049
James Rose <jyrose@verlet.io>
2020-09-01 21:54:09 -0700

    Functioning GUI

--- main.py
+++ main.py
@@ -1,46 +1,205 @@
+import os
 import tkinter as tk
 from tkinter import ttk
-
-
+from PIL import Image
+from tda_options import option_main
 
 
 class GUI:
     def __init__(self):
+        self.gui()
+        self.symbol = None
+        self.apiKey = None
+        self.puts = None
+        self.call = None
+        self.put_options = None
+        self.call_options = None
+        self.colcalc = None
+        self.rightLeft = None
+
+    def gui(self):
+        """
+        GUI
+
+        GUI Inputs:
+        Symbol: ticker symbol of stock desired
+        API Key: TDA API key
+        Left or Right Calc: left is calculate descending strike prices
+        Column: Column to perform the calculations on
+
+        Call: Call chains
+        Puts: Puts Chain
+
+        :return:
+        """
         gui = tk.Tk()
         gui.title('Options Map Graph')
-        gui.geometry('800x500')
+        gui.geometry('870x200')
+        # gui.configure(background='black')
         # input symbol
-        tk.Label(gui, text="Symbol").grid(row=0)
+        tk.Label(gui,
+                 text="Symbol: ",
+                 anchor="e"
+                 ).grid(row=0,
+                        column=0,
+                        padx=(50, 20),
+                        pady=(10, 10)
+                        )
         self.symbol = tk.Entry(gui)
-        self.symbol.grid(row=0, column=1)
+        self.symbol.insert(0,
+                           "AMD"
+                           )
+        self.symbol.grid(row=0,
+                         column=1
+                         )
 
-        tk.Label(gui, text="API Key").grid(row=0, column=2)
+        tk.Label(gui,
+                 text="API Key: ",
+                 anchor="e"
+                 ).grid(row=0,
+                        column=2
+                        )
         self.apiKey = tk.Entry(gui)
-        self.apiKey.grid(row=0, column=3)
+        self.apiKey.grid(row=0,
+                         column=3,
+                         sticky=tk.W+tk.E,
+                         columnspan=4
+                         )
 
         # dropdowns
-        tk.Label(gui, text="Call or Puts :").grid(row=1, column=0)
+        tk.Label(gui,
+                 text="Left or Right Calc :"
+                 ).grid(row=1,
+                        column=0,
+                        padx=(50, 20),
+                        pady=(10, 10)
+                        )
         n = tk.StringVar()
-        self.putCall = ttk.Combobox(gui, width=10, textvariable=n, values=["call", "put"])
-        self.putCall.grid(row=1, column=1)
-
-        tk.Label(gui, text="Left or Right Calc :").grid(row=1, column=2)
-        n = tk.StringVar()
-        self.rightLeft = ttk.Combobox(gui, width=10, textvariable=n, values=["right", "left"])
-        self.rightLeft.grid(row=1, column=3)
-
-        tk.Label(gui, text="Column :").grid(row=1, column=4)
-        n = tk.StringVar()
-        self.colcalc = ttk.Combobox(gui, width=10, textvariable=n, values=["bid", "ask", 'mark', 'last'])
-        self.colcalc.grid(row=1, column=5)
+        self.rightLeft = ttk.Combobox(gui,
+                                      textvariable=n,
+                                      values=["right", "left"]
+                                      )
+        self.rightLeft.grid(row=1,
+                            column=1
+                            )
+        tk.Label(gui,
+                 text="Column :"
+                 ).grid(row=1,
+                        column=2
+                        )
+        o = tk.StringVar()
+        self.colcalc = ttk.Combobox(gui,
+                                    textvariable=o,
+                                    values=["bid", "ask", 'mark', 'last']
+                                    )
+        self.colcalc.grid(row=1,
+                          column=3
+                          )
         # submit button
-        tk.Button(gui, text="submit", command=self.query_data).grid(row=2, column=4, sticky=tk.W, pady=4)
+        tk.Button(gui,
+                  text="Run Query",
+                  command=self.query_data
+                  ).grid(row=2,
+                         sticky=tk.W+tk.E,
+                         columnspan=6
+                         )
+        # results
+        tk.Label(gui,
+                 text="Call:"
+                 ).grid(row=3,
+                        column=0,
+                        padx=(50, 20),
+                        pady=(10, 10)
+                        )
+        p = tk.StringVar()
+        self.call_options = ttk.Combobox(gui,
+                                         width=25,
+                                         textvariable=p,
+                                         values=[]
+                                         )
+        self.call_options.grid(row=3,
+                               column=1
+                               )
+        tk.Button(gui,
+                  text="Show Call",
+                  command=self.show_call
+                  ).grid(row=4,
+                         sticky=tk.W+tk.E,
+                         columnspan=3
+                         )
 
+        tk.Label(gui,
+                 text="Puts:"
+                 ).grid(row=3,
+                        column=3
+                        )
+        q = tk.StringVar()
+        self.put_options = ttk.Combobox(gui,
+                                        width=25,
+                                        textvariable=q,
+                                        values=[]
+                                        )
+        self.put_options.grid(row=3,
+                              column=4
+                              )
+        tk.Button(gui,
+                  text="Show Put",
+                  command=self.show_put
+                  ).grid(row=4,
+                         column=3,
+                         sticky=tk.W+tk.E,
+                         columnspan=3
+                         )
 
         gui.mainloop()
+        return self
 
     def query_data(self):
         """
-
+        Requests data from TDA API.
+        Formats them in  qualified dataframes and converts it to a PNG file
+        Stored locally in a temporary folder
+        Returns file paths ond option description
         """
+        if self.rightLeft.get() == "right":
+            rl = True
+        else:
+            rl = False
+        self.call, self.puts = option_main(self.symbol.get(),
+                                           self.apiKey.get(),
+                                           col=self.colcalc.get(),
+                                           rightLeft=rl
+                                           )
+        self.call_options['values'] = list(self.call.keys())
+        self.put_options['values'] = list(self.puts.keys())
+        return self
 
+    def show_put(self):
+        """
+        Opens window containing the options graph for selected put
+        :return:
+        """
+        tmp_dir = os.path.join(os.getcwd(), ".tmp")
+        Image.open(os.path.join(tmp_dir,
+                                self.puts[self.put_options.get()]
+                                )
+                   ).resize((1000, 1000),
+                            Image.ANTIALIAS
+                            ).show()
+
+    def show_call(self):
+        """
+        Opens window containing the options graph for selected call
+        :return:
+        """
+        tmp_dir = os.path.join(os.getcwd(), ".tmp")
+        Image.open(os.path.join(tmp_dir,
+                                self.call[self.call_options.get()]
+                                )
+                   ).resize((1000, 1000),
+                            Image.ANTIALIAS
+                            ).show()
+
+
+if __name__ == "__main__":
+    GUI()
--- tda_options.py
+++ tda_options.py
@@ -1,19 +1,25 @@
-import requests
 import os
-import io
+import requests
 import pandas as pd
-import time
+import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import matplotlib.pyplot as plt
-from PIL import Image
+
 
 
 def get_data(symbol, apiKey):
+    """
+    Gets data from TDA API
+
+    :param symbol: ticker symbol (str)
+    :param apiKey: TDA API key (str)
+    :return: option chains (dict)
+    """
     endpoint = f"https://api.tdameritrade.com/v1/marketdata/chains?apikey={apiKey}"
     payload = {"symbol": symbol,
                "contractType": "ALL",
-               "strikeCount": 20,
+               "strikeCount": 10,
                "includeQuotes": False,
                "strategy": "SINGLE",
                "range": "ALL",
@@ -25,6 +31,13 @@ def get_data(symbol, apiKey):
 
 
 def create_dataframe(data, asc):
+    """
+    Extracts data from the TDA json dict.
+
+    :param data: TDA options chain (dict)
+    :param asc: True for oscending strike price (bool)
+    :return:  pandas.DataFrame
+    """
     df = pd.DataFrame()
     for data in data.values():
         for key in list(data.keys()):
@@ -41,11 +54,17 @@ def create_dataframe(data, asc):
 
 
 def sub_frame_calc(dft, col):
+    """
+    Calculating the change in price for options
+
+    :param dft: single strike price option chain (pandas.DataFrame)
+    :param col:  column that calc should be performed on (str)
+    :return:  pandas.DataFrame
+    """
     dft_list = dft.groupby("experiation")
-    change_func = lambda x: (x.values[1] - x.values[0]) / x.values[0]
     results = []
     for k, v in dft_list:
-        v["change"] = v[col].rolling(window=2).apply(change_func)
+        v["change"] = (v[col] - v[col].shift(1)) / v[col].shift(1)
         results += [v[["strike",
                        "description",
                        "change",
@@ -53,64 +72,81 @@ def sub_frame_calc(dft, col):
                        col]
                     ].rename(columns={col: "price"})
                     ]
+
     return results
 
 
 def create_plot(dfp):
-    dfp.dropna(inplace=True)
-    plt.figure(figsize=(15, 15))
-    fig, ax1 = plt.subplots(figsize=(15,15))
-    ax2 = ax1.twinx()
+    """
+    Converts data into plots for GUI use
 
+    :param dfp: pandas.DataFrame
+    :return: os.path
+    """
+    dfp.dropna(inplace=True)
+    plt.figure(figsize=(30, 30))
+    fig, ax1 = plt.subplots(figsize=(30, 30))
+    ax2 = ax1.twinx()
     itm = dfp[dfp.inTheMoney == True].strike
     otm = dfp[dfp.inTheMoney == False].strike
     ax1.plot(dfp.strike, dfp.price)
-    ax2.plot(dfp.strike, dfp.change  * 100, color="black")
+    ax2.plot(dfp.strike, dfp.change * 100, color="black")
     ax1.axvspan(itm.min(), itm.max(), color='green', alpha=0.25)
     ax1.axvspan(otm.min(), otm.max(), color='red', alpha=0.25)
     for i in dfp.index:
         ax2.text(dfp.loc[i, "strike"],
                  dfp.loc[i, "change"] * 100,
                  f"{round(dfp.loc[i, 'change'] * 100, 2)}%",
-                 size=16
+                 size=32
                  )
     plt.title(dfp.description.values[0],
-              size=24
+              size=48
               )
     ax1.set_xlabel("strike price",
-                   size=16
+                   size=32
                    )
     ax1.set_ylabel("contract price",
-                   size=16
+                   size=32
                    )
     ax2.set_ylabel("% Change in Price",
-                   size=16
+                   size=32
                    )
     ax1.set_xlim((dfp.strike.min(),
                   dfp.strike.max())
                  )
+    ax1.tick_params(axis='both', which='major', labelsize=32)
+    ax2.tick_params(axis='both', which='major', labelsize=32)
 
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-    im = Image.open(buf)
-    return im
+    save_dir = os.path.join(os.getcwd(),
+                            ".tmp",
+                            f"{np.random.randint(1e15)}.png")
+    plt.savefig(save_dir, format='png')
+    plt.close()
+    return save_dir
 
 
 def option_main(symbol, apiKey, col="bid", rightLeft=True):
+    """
+    Queries data and calculates option chains change in price and plots it
+
+    :param symbol: ticker symbol (str)
+    :param apiKey:  TDA API key (str)
+    :param col:  column for calculation (str)
+    :param rightLeft:  left is ascending strike price (bool)
+    :return: dict
+    """
+    tmp_dir = os.path.join(os.getcwd(), '.tmp')
+    os.system(f"rm -rf {tmp_dir}; mkdir {tmp_dir}")
     content = get_data(symbol, apiKey)
     calls = content["callExpDateMap"]
     puts = content["putExpDateMap"]
     calldf = sub_frame_calc(create_dataframe(calls, rightLeft), col)
     putsdf = sub_frame_calc(create_dataframe(puts, rightLeft), col)
+
     call_plt = {}
     put_plt = {}
-    for i in range(len(calldf)):
-        dfp = calldf[i]
-        descript = dfp.description.values[0]
-        call_plt[descript] = create_plot(dfp)
-    for i in range(len(putsdf)):
-        dfp = putsdf[i]
-        descript = dfp.description.values[0]
-        put_plt[descript] = create_plot(dfp)
+    for call in calldf:
+        call_plt[call.description.values[0]] = create_plot(call)
+    for put in putsdf:
+        put_plt[put.description.values[0]] = [create_plot(put)]
     return call_plt, put_plt
